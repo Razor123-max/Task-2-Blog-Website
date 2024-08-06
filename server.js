cb3737aa36fdf1e19e6511df2ec2ece3815d5922
Razor123-max <111220437+Razor123-max@users.noreply.github.com>
2024-08-06 23:37:07 +0530

    made some update with index, adced new ppages for frontend

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const articleRouter = require("./routes/articles")
+const mongoose = require('mongoose');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+const articles = [{
+  title: 'Test Article 1',
+  createdAt: Date.now(),
+  description: 'Test Description'
+},
+{
+  title: 'Test Article 2',
+  createdAt: Date.now(),
+  description: 'Test Description'
+}]
+  res.render('index', {articles:articles});
+})
+
+
+app.use('/articles', articleRouter);
+
+app.listen(3000);
